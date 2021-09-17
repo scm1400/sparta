369a8a84e36103bc4065ef3f73e7c82fa2540525
Muice <scm1400@gmail.com>
2021-09-17 19:10:51 +0900

    타임어택-fail

--- time_attack/week1/app.py
+++ time_attack/week1/app.py
@@ -15,10 +15,12 @@ def show_list():
     groups = list(db.codes.distinct("group"))
     return jsonify(groups)
 
-@app.route('/codes', methods=['GET'])
-def show_list():
+@app.route('/codes', methods=['POST'])
+def post_api():
     groups = list(db.codes.distinct("group"))
     return jsonify(groups)
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
