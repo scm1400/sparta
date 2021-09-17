95be15b155a9675668fad12089843842cd0f5dfa
Muice <scm1400@gmail.com>
2021-09-17 18:55:30 +0900

    타임어택 실패

---
+++ time_attack/week1/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify, request
+from datetime import datetime
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbStock
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/base/codes', methods=['GET'])
+def show_list():
+    groups = list(db.codes.distinct("group"))
+    return jsonify(groups)
+
+@app.route('/codes', methods=['GET'])
+def show_list():
+    groups = list(db.codes.distinct("group"))
+    return jsonify(groups)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
