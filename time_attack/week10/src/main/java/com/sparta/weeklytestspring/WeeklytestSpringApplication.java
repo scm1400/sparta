10bbbba1d8f62a0b36f2a73fbc01adc288370a4c
Muice <scm1400@gmail.com>
2021-12-03 18:23:58 +0900

    week10

---
+++ time_attack/week10/src/main/java/com/sparta/weeklytestspring/WeeklytestSpringApplication.java
@@ -0,0 +1,22 @@
+package com.sparta.weeklytestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class WeeklytestSpringApplication {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.properties";
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(WeeklytestSpringApplication.class).properties(APPLICATION_LOCATIONS).run(args);
+
+    }
+
+}
---
+++ time_attack/week10/src/main/java/com/sparta/weeklytestspring/dto/ArticleRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.weeklytestspring.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ArticleRequestDto {
+    private String title;
+    private String content;
+    private String tags;
+    private String filePath;
+}
